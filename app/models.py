d7ff1d8c5e79bfd805ecaaa0e98444b387762797
Ibi <ibieldahuk@gmail.com>
2021-06-15 16:48:36 -0300

    Agregando la columna timestamp
    Agregué la columna timestamp y lo necesario para que la platilla index imprima el fecha de publicación de los posts que se creen a partir de ahora.

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
     body = db.Column(db.Text())
-    t1m3 = db.Column(db.DateTime, default=datetime.now)
+    timestamp = db.Column(db.DateTime(), default=datetime.now)
 
     def __repr__(self) -> str:
         return f'<post> {self.title}'
