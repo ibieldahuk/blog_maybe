8a153785b6cbf516a4dee4767778478ace06a86d
Ibi <ibieldahuk@gmail.com>
2021-06-15 15:21:38 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from app import db
+from datetime import datetime
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
     body = db.Column(db.Text())
+    t1m3 = db.Column(db.DateTime, default=datetime.now)
 
     def __repr__(self) -> str:
         return f'<post> {self.title}'
