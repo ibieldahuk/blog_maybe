1d82312e66feb7883fe60afe70c224efffedb6f6
Ibi <ibieldahuk@gmail.com>
2021-07-03 16:49:50 -0300

    Validators
    Agregué validación a los campos de titulo y cuerpo de los formularios de publicación y edición.

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,15 @@
-from wtforms import Form, StringField, TextAreaField, SubmitField
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
 
 
-class CreatePostForm(Form):
-    title = StringField('Título')
-    body = TextAreaField('Cuerpo')
+class CreatePostForm(FlaskForm):
+    title = StringField('Título', [DataRequired('Complete este campoOoOo.')])
+    body = TextAreaField('Cuerpo', [DataRequired('Complete este campoOoOo.')])
     submit = SubmitField('Publicar')
 
 
-class EditPostForm(Form):
-    title = StringField('Título')
-    body = TextAreaField('Cuerpo')
+class EditPostForm(FlaskForm):
+    title = StringField('Título', [DataRequired('Complete este campoOoOo.')])
+    body = TextAreaField('Cuerpo', [DataRequired('Complete este campoOoOo.')])
     submit = SubmitField('Editar')
